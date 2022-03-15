18d1f58da1216c39097ae12cb7d24bb7f8791abe
Moritz Hoffmann <clonker@gmail.com>
2022-03-15 18:35:57 +0100

    f

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,14 @@
 from conans import ConanFile
 
-
-class CTiPRDTests(ConanFile):
+class CTiPRD(ConanFile):
     options = {}
-    name = "CTiPRDTests"
+    name = "CTiPRD"
     version = "0.1"
     requires = (
+        "pybind11/2.9.1",
+        "spdlog/1.9.2",
+
         "catch2/2.13.7"
     )
     generators = "cmake", "gcc", "txt", "cmake_find_package"
+
