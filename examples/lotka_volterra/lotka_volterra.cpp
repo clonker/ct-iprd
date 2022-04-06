42e30db0db7d73bdfcaa70bddfd2d680adade07f
Moritz Hoffmann <clonker@gmail.com>
2022-04-06 16:25:29 +0200

    un huh

--- examples/lotka_volterra/lotka_volterra.cpp
+++ examples/lotka_volterra/lotka_volterra.cpp
@@ -14,10 +14,10 @@ using np_array = ctiprd::binding::np_array<T, shape...>;
 using System = ctiprd::systems::LotkaVolterra<float>;
 
 NB_MODULE(lv_mod, m) {
-
+    ctiprd::binding::exportBaseTypes<System::dtype>(m);
     ctiprd::binding::exportSystem<System>(m, "LotkaVolterra");
 
-    m.def("simulate", [] (std::size_t nSteps, float dt, int njobs, std::function<void(std::size_t)> progressCallback) {
+    m.def("simulate", [] (std::size_t nSteps, float dt, int njobs, const std::function<void(std::size_t)>& progressCallback) {
         System system {};
         auto pool = ctiprd::config::make_pool(njobs);
         auto integrator = ctiprd::integrator::EulerMaruyama{system, pool};
--- include/ctiprd/binding/system_bindings.h
+++ include/ctiprd/binding/system_bindings.h
@@ -18,8 +18,15 @@ namespace nb = nanobind;
 template<typename dtype, std::size_t... shape>
 using np_array = nb::tensor<dtype, nb::shape<shape...>, nb::c_contig, nb::device::cpu>;
 
-template<typename System, typename Module>
-void exportSystem(Module &module, std::string_view name) {
+template<typename dtype>
+void exportBaseTypes(nb::module_ &module) {
+    nb::class_<ParticleType<dtype>>(module, "ParticleType")
+        .template def_readonly("name", &ParticleType<dtype>::name)
+        .template def_readonly("diffusion_constant", &ParticleType<dtype>::diffusionConstant);
+}
+
+template<typename System>
+void exportSystem(nb::module_ &module, std::string_view name) {
     std::string strName {name};
     auto clazz = nb::class_<System>(module, strName.c_str());
     clazz.template def_readonly_static("periodic", &System::periodic);
@@ -29,6 +36,8 @@ void exportSystem(Module &module, std::string_view name) {
         std::copy(begin(self.boxSize), end(self.boxSize), reinterpret_cast<typename System::dtype*>(out.data()));
         return out;
     });
+    clazz.template def_readonly_static("particle_types", &System::types);
+
 }
 
 }
