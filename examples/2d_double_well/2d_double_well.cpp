b35ecb8b74f56a79bc62e7e7e31172f5b7833baf
Moritz Hoffmann <clonker@gmail.com>
2022-04-11 13:39:45 +0200

    f

--- examples/2d_double_well/2d_double_well.cpp
+++ examples/2d_double_well/2d_double_well.cpp
@@ -2,14 +2,16 @@
 // Created by mho on 3/15/22.
 //
 
+#include <ctiprd/config.h>
 #include <ctiprd/systems/double_well.h>
+#include <ctiprd/cpu/integrators/EulerMaruyama.h>
 
 using System = ctiprd::systems::DoubleWell<float>;
 
 int main() {
     auto pool = ctiprd::config::make_pool(5);
     System system {};
-    auto integrator = ctiprd::integrator::EulerMaruyama<System>{system, pool};
+    auto integrator = ctiprd::cpu::integrator::EulerMaruyama<System>{system, pool};
     integrator.particles()->addParticle({{0., 0.}}, "A");
     integrator.step(1e-3);
     return 0;
