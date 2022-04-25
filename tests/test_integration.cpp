6f48ade29fa0a88c94d5b1d4d43ce6bae0f82f87
Moritz Hoffmann <clonker@gmail.com>
2022-04-25 10:27:11 +0200

    f

--- tests/test_integration.cpp
+++ tests/test_integration.cpp
@@ -11,15 +11,10 @@ TEMPLATE_TEST_CASE("Test Integration", "[integration]", ctiprd::systems::DoubleW
     std::size_t nSteps = 100;
 
     TestType system {};
-    auto pool = ctiprd::config::make_pool(3);
+    auto pool = ctiprd::config::make_pool(10);
     auto integrator = ctiprd::cpu::integrator::EulerMaruyama{system, pool};
-    for(int n = 0; n < 500; ++n) {
-        integrator.particles()->addParticle({{0., 0.}}, system.types[0].name);
-    }
-    for(int n = 0; n < 500; ++n) {
-        integrator.particles()->addParticle({{0., 0.}}, system.types[1].name);
-    }
-
+    integrator.particles()->initializeParticles(5000, system.types[0].name);
+    integrator.particles()->initializeParticles(5000, system.types[1].name);
 
     spdlog::set_level(spdlog::level::debug);
     for(std::size_t t = 0; t < nSteps; ++t) {
